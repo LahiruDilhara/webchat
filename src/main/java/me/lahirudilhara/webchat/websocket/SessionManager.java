16e93677312473a1d0ce1ae83fb490c1d0d59a1b
lahirukasunidilhara <galahirudilhara@gmail.com>
2025-08-18 18:55:30 +0530

    move the session storing to the session manager

--- src/main/java/me/lahirudilhara/webchat/websocket/SessionManager.java
+++ src/main/java/me/lahirudilhara/webchat/websocket/SessionManager.java
@@ -1,27 +1,72 @@
 package me.lahirudilhara.webchat.websocket;
 
+import me.lahirudilhara.webchat.entities.WebSocketUserSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class SessionManager {
-    private final SessionRegistry sessionRegistry;
-    private final WebChatController webChatController;
+    private static final Logger log = LoggerFactory.getLogger(SessionManager.class);
 
-    public SessionManager(SessionRegistry sessionRegistry, WebChatController webChatController) {
-        this.sessionRegistry = sessionRegistry;
-        this.webChatController = webChatController;
+
+    private final Map<String, WebSocketUserSession> webSocketUsers;
+
+    public SessionManager() {
+        webSocketUsers = new ConcurrentHashMap<>();
+
     }
 
-    public void onUserConnect(String username, WebSocketSession session){
-        this.sessionRegistry.addUser(username, session);
+    public void addWebSocketSession(WebSocketSession session) {
+        WebSocketUserSession webSocketUserSession = new WebSocketUserSession(session.getPrincipal().getName(), Instant.now(),session);
+        webSocketUsers.put(session.getPrincipal().getName(), webSocketUserSession);
     }
 
-    public void onUserDisconnect(String username, WebSocketSession session){
-        this.sessionRegistry.findAndRemoveUser(username);
+    public void removeUser(String username){
+        this.webSocketUsers.remove(username);
     }
 
-    public void onUserMessage(String message, WebSocketSession session){
-        webChatController.onMessage(message,session.getPrincipal().getName());
+    public boolean isUserOnline(String username){
+        WebSocketUserSession userSession = this.webSocketUsers.get(username);
+        if(userSession == null){
+            return false;
+        }
+        if(!userSession.getSession().isOpen()){
+            return false;
+        }
+        return true;
+    }
+
+    public WebSocketUserSession getUserByUsername(String username){
+        return this.webSocketUsers.get(username);
+    }
+
+    public List<String> getActiveUsers(){
+        return new ArrayList<>(this.webSocketUsers.keySet());
+    }
+
+    public void sendMessageToSession(String username, String message){
+        WebSocketUserSession webSocketUserSession = this.webSocketUsers.get(username);
+        if(webSocketUserSession == null){
+            return;
+        }
+        if(!webSocketUserSession.getSession().isOpen()){
+            log.error("Session has been closed for the user {}",username);
+            return;
+        }
+        try{
+            webSocketUserSession.getSession().sendMessage(new TextMessage(message));
+        }
+        catch(Exception e){
+            log.error("Error while sending data to session. the user name is {}",username, e);
+        }
     }
 }
