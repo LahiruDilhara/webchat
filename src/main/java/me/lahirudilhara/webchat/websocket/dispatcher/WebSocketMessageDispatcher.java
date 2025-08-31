995809cc28ed91e9d75497672ac8d7c18d2ccb21
lahirukasunidilhara <galahirudilhara@gmail.com>
2025-08-31 19:10:15 +0530

    config the dispatcher to listen for the client messages and call for the appropriate handler

--- src/main/java/me/lahirudilhara/webchat/websocket/dispatcher/WebSocketMessageDispatcher.java
+++ src/main/java/me/lahirudilhara/webchat/websocket/dispatcher/WebSocketMessageDispatcher.java
@@ -1,7 +1,13 @@
 package me.lahirudilhara.webchat.websocket.dispatcher;
 
 import me.lahirudilhara.webchat.dto.wc.WebSocketMessageDTO;
+import me.lahirudilhara.webchat.websocket.events.ClientExceptionEvent;
+import me.lahirudilhara.webchat.websocket.events.OnClientMessageEvent;
 import me.lahirudilhara.webchat.websocket.handlers.MessageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -10,9 +16,12 @@ import java.util.Map;
 
 @Service
 public class WebSocketMessageDispatcher {
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private static final Logger log = LoggerFactory.getLogger(WebSocketMessageDispatcher.class);
     private final Map<Class<? extends WebSocketMessageDTO>, MessageHandler<? extends WebSocketMessageDTO>> userMessageHandlerMap = new HashMap<>();
 
-    public WebSocketMessageDispatcher(List<MessageHandler<? extends WebSocketMessageDTO>> messageHandlers) {
+    public WebSocketMessageDispatcher(List<MessageHandler<? extends WebSocketMessageDTO>> messageHandlers, ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
         for (MessageHandler<? extends WebSocketMessageDTO> messageHandler : messageHandlers) {
             userMessageHandlerMap.put(messageHandler.getMessageType(), messageHandler);
         }
@@ -21,8 +30,15 @@ public class WebSocketMessageDispatcher {
     public <T extends WebSocketMessageDTO> void dispatch(T message, String senderUsername) {
         MessageHandler<T> messageHandler = (MessageHandler<T>) userMessageHandlerMap.get(message.getClass());
         if (messageHandler == null) {
-            throw new IllegalArgumentException("No handler found for"+ messageHandler.getClass().getName());
+            log.error("The message handler not found. The username is {}. The message class is {}",senderUsername,message.getClass());
+            applicationEventPublisher.publishEvent(new ClientExceptionEvent(senderUsername,"Internal error occurred"));
+            return;
         }
         messageHandler.handleMessage(message,senderUsername);
+    }
+
+    @EventListener
+    public void OnClientMessage(OnClientMessageEvent event){
+        this.dispatch(event.getMessageDTO(),event.getUsername());
     }
 }
