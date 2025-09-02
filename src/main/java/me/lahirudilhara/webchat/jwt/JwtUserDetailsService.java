ca81e27d74984fc46cd9734af1525ccfbb6327da
lahirukasunidilhara <galahirudilhara@gmail.com>
2025-09-02 12:43:41 +0530

    solve the jwt cycle dependency problem and add cache enabled user fetch

--- src/main/java/me/lahirudilhara/webchat/jwt/JwtUserDetailsService.java
+++ src/main/java/me/lahirudilhara/webchat/jwt/JwtUserDetailsService.java
@@ -3,6 +3,7 @@ package me.lahirudilhara.webchat.jwt;
 import me.lahirudilhara.webchat.models.User;
 import me.lahirudilhara.webchat.repositories.UserRepository;
 import me.lahirudilhara.webchat.service.api.UserService;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,15 +11,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
-    private final UserService userService;
+    private final UserRepository userRepository;
 
-    public JwtUserDetailsService(UserService userService) {
-        this.userService = userService;
+    public JwtUserDetailsService( UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.getUser(username);
+        User user = getUser(username);
 
         if (user == null) {
             System.out.println("User not found");
@@ -27,5 +28,10 @@ public class JwtUserDetailsService implements UserDetailsService {
 
         UserDetails userDetails = new SecureUserDetails(user);
         return userDetails;
+    }
+
+    @Cacheable(value = "authUser",key = "#username")
+    public User getUser(String username){
+        return userRepository.findByUsername(username);
     }
 }
