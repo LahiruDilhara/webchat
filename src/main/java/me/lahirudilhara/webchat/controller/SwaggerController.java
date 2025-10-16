529bf7f227a8523e095092879fcccbbe192d7ce1
lahirudilhara <galahirudilhara@gmail.com>
2025-10-16 17:08:34 +0530

    added the swagger

---
+++ src/main/java/me/lahirudilhara/webchat/controller/SwaggerController.java
@@ -0,0 +1,22 @@
+package me.lahirudilhara.webchat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SwaggerController {
+
+
+//    @GetMapping("/api/")
+//    public String redirectToSwaggerUI() {
+//        return "forward:/swagger-ui/index.html";
+//    }
+//
+//    @GetMapping("/api")
+//    public String redirectToSwaggerUIWithSlash() {
+//        return "forward:/swagger-ui/index.html";
+//    }
+}
